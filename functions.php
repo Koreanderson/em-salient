d0cf5a444a16ccdb784d6a16ce1e842cdac47ba5
brian <koreanderson@gmail.com>
2016-12-29 13:38:10 -0500

    enqueue compiled styles to theme

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php 
-
 add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles');
 function salient_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'));
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'));
+	wp_enqueue_style( 'compiled-styles', get_stylesheet_directory_uri() . '/assets/css/main.css', array('parent-style'));
 }
-
 ?>
